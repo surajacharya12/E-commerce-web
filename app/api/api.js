7b97cf53d705d95b91dfc8a11bff85a4b3dbed72
surajacharya12 <surajacharya.080@kathford.edu.np>
2025-10-08 13:29:21 +0545

    pre

--- app/api/api.js
+++ app/api/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001" || "https://e-commerce-backend-ar1h.vercel.app";
 
 export default API_URL;
