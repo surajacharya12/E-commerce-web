aea273f70e1caafe48c9b0356f1425ad19c4e01c
surajacharya12 <surajacharya.080@kathford.edu.np>
2025-10-08 14:03:16 +0545

    dep

--- app/api/api.js
+++ app/api/api.js
@@ -1,3 +1,8 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001" || "https://e-commerce-backend-ar1h.vercel.app";
+// src/config/api.js (or similar)
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://e-commerce-backend-ar1h.vercel.app"
+    : "http://localhost:3001");
 
 export default API_URL;
